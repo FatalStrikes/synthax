2f89f361addfbae940504f50c4c2725f371803cb
fatalstrikes <fatalstrikes@gmail.com>
2015-06-02 22:43:45 -0500

    initial testing if statements

--- synthax/Program.cs
+++ synthax/Program.cs
@@ -90,10 +90,99 @@ namespace synthax
 			main.node_content = tokens_list [node_index];
 			expect (TknType.TKN_LBRACK);
 			Node temp = nodeDeclarationList ();
-			main.node_brother = temp;
+			main.node_successor[0] = temp;
+			temp = nodeSentenceList ();
+			main.node_successor[1] = temp;
 			Console.WriteLine (node_index);
 			expect (TknType.TKN_RBRACK);
 			return main;
+		}
+
+		public static Node nodeSentenceList(){
+			Node node = new Node ();
+			Token nextToken = tokens_list [++node_index];
+			Node root = new Node ();
+			if (nextToken.type != TknType.TKN_EOF) {
+				root = nodeSentence ();
+			}else{
+				return null;
+			}
+
+			node = root;
+
+			nextToken = tokens_list [++node_index];
+			while (nextToken.type != TknType.TKN_RBRACK) {
+				Node temp = nodeSentence ();
+
+				root.node_brother = temp;
+				root = temp;
+				nextToken = tokens_list [++node_index];
+			}
+			node_index--;
+			return node;
+		}
+
+		public static Node nodeSentence(){
+			Node node = new Node ();
+			Token t = tokens_list [node_index];
+			Node temp = new Node ();
+			switch (t.type) {
+
+			case TknType.TKN_IF:
+				temp = nodeIf ();
+				break;
+			case TknType.TKN_THEN:
+				temp = nodeThen ();
+				break;
+//			case TknType.TKN_WRITE:
+			case TknType.TKN_READ:	
+				temp = nodeRead ();
+				break;
+			
+
+			default:
+				break;
+			}
+
+
+			node = temp;
+			return node;
+		}
+
+		public static Node nodeIf(){
+			Node node = new Node ();
+			Token t = tokens_list [node_index];
+			node.node_content = t;
+			expect (TknType.TKN_LPAREN);
+			// TODO: b-expresions
+			expect (TknType.TKN_RPAREN);
+//			expect (TknType.TKN_THEN);
+//			expect (TknType.TKN_LBRACK);
+//			// TODO: lista de sentencias
+//			expect (TknType.TKN_RBRACK);
+//			//TODO: ELSE
+//			expect(TknType.TKN_FI);
+			return node;
+		}
+
+		public static Node nodeThen(){
+			Node node = new Node ();
+			node.node_content = tokens_list [node_index];
+			Token t = tokens_list [node_index];
+			expect (TknType.TKN_LBRACK);
+			// TODO: lista de sentencias
+			expect (TknType.TKN_RBRACK);
+			return node;
+		}
+
+		public static Node nodeRead(){
+			Node node = new Node ();
+			Token t = tokens_list [node_index];
+			node.node_content = t;
+			expect (TknType.TKN_ID);
+			node.node_successor [0] = new Node (tokens_list [node_index]);
+			expect (TknType.TKN_SEMICOLON);
+			return node;
 		}
 
 		public static Node nodeDeclarationList(){
@@ -125,54 +214,13 @@ namespace synthax
 		public static Node nodeDeclaration(){
 			Node node = new Node ();
 			node.node_content = tokens_list [node_index];;
-			expect (TknType.TKN_ID);
+			if (!expect (TknType.TKN_ID)) {
+				return node;
+			}
 			node.node_successor [0] = new Node (tokens_list [node_index]);
 			return node;
 		}
-			
 
-//		public static Node nodeProgram (){
-//			Node main = new Node ();
-//			main.node_content = tokens_list [node_index];
-//			Console.WriteLine ("-> program");
-//			expect (TknType.TKN_LBRACK);
-//			main.node_successor = nodeListaDeDeclaraciones ();
-//			// main.node_successor2 = nodeListaDeSentencias ();
-//			expect (TknType.TKN_RBRACK);
-//			return main;
-//		}
-//
-//
-//		// CHECK NODO DE DECLARACIONES Y SECUENCIOAS!!!
-//		public static Node nodeListaDeDeclaraciones(){
-////			Console.WriteLine ("NODO LISTA DE DECLARACIONES");
-//			Node node = new Node ();
-//			node.node_content = new Token (TknType.TKN_LISTADEC, "");
-//			while (tokens_list[node_index+1].type > TknType.TKN_FLOAT && tokens_list[node_index+1].type < TknType.TKN_BOOL) {
-//				node.node_successor = nodeDeclaracion ();
-//			}
-//			node.depth++;
-//			return node;
-//		}
-//
-//		public static Node nodeDeclaracion(){
-////			Console.WriteLine ("NODO DELCARACION");
-//			Node node = new Node ();
-//			node.node_content = tokens_list [++node_index];
-//			Console.Write ("-> " + new string(node.node_content.lexema) + "\r\n");
-//			expect (TknType.TKN_ID);
-//			node.node_successor = nodeId ();
-//			expect (TknType.TKN_SEMICOLON);
-//			return node;
-//		}
-//
-//		public static Node nodeId(){
-////			Console.WriteLine ("NODO ID");
-//			Node node = new Node ();
-//			node.node_content = tokens_list [node_index];
-//			Console.Write ("-> " + new string(node.node_content.lexema) + "\r\n");
-//			return node;
-//		}
 
 		public static bool expect(TknType t){
 			if (tokens_list [++node_index].type == t) {
@@ -192,23 +240,21 @@ namespace synthax
 
 
 		public static void printTree (Node tree){
-//			for (int i = 0; i < tree.depth; i++) {
-//				Console.Write ("-");
-//			}
 			if (tree != null) {
 
-
 				if (tree.node_content != null) {
-					Console.Write ("->" + tree.node_content.type + "  " + new string (tree.node_content.lexema) + "\r\n");
+					Console.WriteLine ("->" + tree.node_content.type + "  " + new string (tree.node_content.lexema) + "\r\n");
 				}
 
 				if (tree.node_successor != null) {
+					Console.WriteLine ("Printing succesor");
 					foreach (var item in tree.node_successor) {
 						printTree (item);
 					}
 				}	
+
 				if (tree.node_brother != null) {
-				
+					Console.WriteLine ("Printing brother");
 					printTree (tree.node_brother);
 				}
 
